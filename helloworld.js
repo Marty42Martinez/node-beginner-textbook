576526026cbe115010122ed7ba2be5f07bc7a89f
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-14 10:24:33 -0700

    functions as parameters

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
+
 console.log('Hello World');
+
+function say(word) {
+  console.log(word);
+}
+
+function execute(someFunction, value) {
+  someFunction(value);
+}
+
+execute(say, "hello");
+execute(function(word) { console.log(word) }, "Hi there!");

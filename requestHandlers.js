acee1624bf69076249190f2d7799cbd5ad5cded1
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-14 19:23:42 -0700

    purposefully coding something incorrectly...

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
   console.log('Request handler START was called.');
+  return 'Hello Start';
 }
 
 function upload() {
   console.log('Request handler for UPLOAD was called');
+  return 'Hello Upload';
 }
 
 module.exports = {

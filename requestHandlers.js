caa7edb2fb8419d0249942c134d0326f2905ca53
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-14 19:51:42 -0700

    still blocking, but faster operation

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,12 @@
+const exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler START was called.');
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while(new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return 'Hello Start';
+  let content = 'empty';
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  })
+  return content;
 }
 
 function upload() {
